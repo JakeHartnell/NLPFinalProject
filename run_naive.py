afa489e6111b633e0a1ac3c2a24f1d3e32bc817d
tristantao <tao.tristan@gmail.com>
2013-12-03 14:51:19 -0800

    updated the run_naive.py

--- run_naive.py
+++ run_naive.py
@@ -1,7 +1,13 @@
 import naive_bayes as nb
+import nltk
 
-bad_training = nb.get_comment_data('bad.txt', -1)
+bad_training_raw = nb.get_comment_data('bad.txt', -1)
+bad_training_features = [(nb.get_features(comment), val) for (comment, val) in bad_training_raw.items()]
 
-good_training = nb.get_comment_data('good.txt' 1)
+#good_training = nb.get_comment_data('good.txt', 1)
+
+classifier = nltk.NaiveBayesClassifier.train(bad_training_features)
+
+print classifier
 
 
