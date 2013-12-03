16bb2bc599c248a0f192bb3fc4e9fd0738b0cd1b
tristantao <tao.tristan@gmail.com>
2013-12-03 15:00:18 -0800

    updated to include good comment; now combines, shuffles, and subsets

--- run_naive.py
+++ run_naive.py
@@ -5,8 +5,16 @@ bad_training_raw = nb.get_comment_data('bad.txt', -1)
 bad_training_features = [(nb.get_features(comment), val) for (comment, val) in bad_training_raw.items()]
 
 #good_training = nb.get_comment_data('good.txt', 1)
+#good_training_features = [(nb.get_features(comment), val) for (comment, val) in good_training_raw.items()]
 
-classifier = nltk.NaiveBayesClassifier.train(bad_training_features)
+combined_feature_sets = bad_training_features +  good_training_features
+
+random.shuffle(combined_feature_sets)
+size = int(len(combined_feature_sets) * 0.9)
+train_set, test_set = combined_feature_sets[size:], combined_feature_sets[:size]
+
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
 
 print classifier
 
