e5bb1cc1fff089941f9f767ae6666730c70e2a1e
dirkwit <dwit@ischool.berkeley.edu>
2013-12-05 14:48:03 -0800

    Added feature
    Feature: Chain of 2 characters in a row indicates that it is probably a
    hipster trying to use too many characters.
    I looooove this ssoonngg!!!! Hihi OneLuv!

--- naive_bayes.py
+++ naive_bayes.py
@@ -53,13 +53,48 @@ def num_one_char_words(comment):
 
     return count
 
+def num_two_char_rep(comment):
+    '''
+    returns the number of two similar characters in a row, such as: ssoonngg (would return 3)
+    '''
+    i = 0
+    count = 0
+    words = comment.split(" ")
+    prevPair = False
+
+    # loop trough the words in the comment
+    for word in words:
+        # loop through the word and evaluate every character
+        # len - 1 because we need to evaluate the next char everytime
+        while i < len(word) - 1:
+            # This boolean stores whether there is a pair of the same characters (such as oo or nn)
+            thisPair = False
+
+            character = word[i]
+            nextchar = word[i+1]
+
+            # See whether the current 2 characters are the same
+            if character == nextchar:
+                thisPair = True
+
+            # If the previous 2 characters where also the same we have a sequence that we should count
+            if thisPair == True and prevPair == True:
+                count +=1
+
+            # The current pair is now the previous pair
+            prevPair = thisPair
+            # increase i with an extra 1 because we want to go to the next pair right away
+
+            i += 2
+
+    return count
+
 def get_features(comment):
     '''
     given a comment, returns the features associated with the comment.
     '''
     features = {}
 
-    #number of 2 char repetition
     #ratio of white space
     #longest length of consecutive char
     #continuoys line breaks
@@ -76,6 +111,8 @@ def get_features(comment):
     features['.com'] = '.com' in comment
     features['badword'] = num_bad_word(comment)
     features['len'] = len(comment)
+    #features['2charchain'] = num_two_char_rep(comment)
+
     #naive and simple, just length to get it working.
     return features
 
